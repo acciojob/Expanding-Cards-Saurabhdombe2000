7e52ea22fabb3b72eaa3bbd949463c7bdb7da05d
Saurabh Dombe <saurabhdombe83@gmail.com>
2025-10-03 12:03:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-const panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll(".panel");
 
-panels.forEach(panel => {
-  panel.addEventListener('click', () => {
-    removeActiveClasses();
-    panel.classList.add('active');
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    // remove active from all
+    panels.forEach((p) => p.classList.remove("active"));
+    // add active to clicked
+    panel.classList.add("active");
   });
 });
-
-function removeActiveClasses() {
-  panels.forEach(panel => {
-    panel.classList.remove('active');
-  });
-}
